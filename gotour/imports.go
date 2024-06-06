b5885594ad6a34b90be4416f7aff24a946f8af24
thenickstrick <57534232+thenickstrick@users.noreply.github.com>
2024-06-06 18:33:04 -0500

    add imports tour

---
+++ gotour/imports.go
@@ -0,0 +1,11 @@
+package main
+
+// this could be done with import "package" line by line, but why type that out all the time?
+import (
+	"fmt"
+	"math"
+)
+
+func main () {
+	fmt.Printf("Now you have %g problems. \n", math.Sqrt(7))
+}
