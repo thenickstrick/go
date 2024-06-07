11d149c5b0b264848b6bd1318f56e53793d6373b
thenickstrick <57534232+thenickstrick@users.noreply.github.com>
2024-06-06 22:17:54 -0500

    add functions-continued gotour

---
+++ gotour/functions-continued.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// when two or more consecutive named function parameters share a type, you omit the type on all but the last
+func add(x, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+}
