0c9bd8680eb5e203ed139e3492a8ca8a169d3faf
thenickstrick <57534232+thenickstrick@users.noreply.github.com>
2024-06-07 10:11:44 -0500

    add comments

--- gotour/variables-with-initializers.go
+++ gotour/variables-with-initializers.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// var declarations can include initializers, one per variable
+// when the initializers are present, type can be omitted as the variable will take the type of the initializer
 var i, j int = 1, 2
 
 func main() {
