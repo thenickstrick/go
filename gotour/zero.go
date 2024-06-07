50898167be0356c62141c84ec3c589da37721b9a
thenickstrick <57534232+thenickstrick@users.noreply.github.com>
2024-06-07 11:46:47 -0500

    add zero gotour

---
+++ gotour/zero.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// variables declared without an explicit initial value are given their "zero value"
+// these "zero values" are:
+	// 0 for numeric types
+	// false for booleans
+	// "" empty string for string types
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
