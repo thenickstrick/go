3790de7115ca1ab726c15fa13cc872431ea52016
thenickstrick <57534232+thenickstrick@users.noreply.github.com>
2024-06-06 19:21:35 -0500

    update imports tour comments

--- gotour/imports.go
+++ gotour/imports.go
@@ -1,6 +1,7 @@
 package main
 
-// this could be done with import "package" line by line, but why type that out all the time?
+// this could be done with import "package" line by line, but why type that out all the time? 
+// use a "factored" import statement
 import (
 	"fmt"
 	"math"
